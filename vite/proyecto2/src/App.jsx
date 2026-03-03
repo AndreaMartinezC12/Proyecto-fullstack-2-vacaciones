6cdf3acca771e7f8f9c6e1e1607c4de956082bd2
AndreaMartinezC12 <andre.martcer@gmail.com>
2026-03-02 19:20:38 -0600

    Creando carpeta de Vite para que sea la base del proyecto

---
+++ vite/proyecto2/src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1>Hola</h1>
+        <p>Como estas?</p>
+      </div>
+
+    </>
+  )
+}
+
+export default App
