026488e729b34769a640c2c1ed520afcafded981
AndreaMartinezC12 <andre.martcer@gmail.com>
2026-03-08 19:13:38 -0600

    Agregando formulario para ingresar los datos sobre el viaje a realizar. La pantalla del formulario es separada de la pantalla que muestra los resultados

--- vite/proyecto2/src/App.jsx
+++ vite/proyecto2/src/App.jsx
@@ -3,21 +3,22 @@ import './App.css'
 import Footer from './components/Footer'
 import FlightSearch from './components/GetFlights'
 import HotelSearch from './components/GetHotels'
+import SearchData from './components/forms/SearchData'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { useState } from 'react'
 
 
 function App() {
-
+  const [destCode, setDestCode] = useState(null)
+  // const [destCode,setDestCode] = useState(null)
   return (
     <>
-      <div>
-        {/* <FlightSearch/> */}
-        <HotelSearch/>
-        {/* <h1>Hola</h1>
-        <p>Como estas?</p> */}
-        {/* <p className="text-7xl text-cyan-600 text-center bg-red-400 shadow-2xl shadow-amber-300">Hola desde tailwind</p> */}
-        
-        
-      </div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<SearchData setDestCode={setDestCode}/>} />
+          <Route path="/results" element={<FlightSearch destCode={destCode}/>}/>
+        </Routes>
+      </BrowserRouter>
 
     </>
   )
---
+++ vite/proyecto2/src/components/forms/SearchData.jsx
@@ -0,0 +1,77 @@
+import React from "react";
+import { useState, useEffect } from "react";
+// import InputDepartDate from "../inputs/InputDepartDate";
+// import InputDestCode from "../inputs/InputDestCode";
+import { useNavigate } from "react-router-dom";
+
+export default function SearchData({setDestCode}){
+    const navigate = useNavigate()
+    //const [destCode, setDestCode] = useState("")
+
+    const doSubmit = (e) =>{
+        e.preventDefault()
+
+        navigate("/results")
+    }
+
+    return(
+        <>
+        <form onSubmit={doSubmit}>
+            <h1>Arma tus vacaciones</h1>
+            <p>A donde quieres viajar?</p>
+            <input
+            type= "text"
+            placeholder= "Nombre de tu destino"
+            className="bg-amber-900"
+            onChange={(e) => setDestination(e.target.value)}
+            />
+
+            <p>Aeropuerto de origen:</p>
+            <input
+            type= "text"
+            placeholder="Aeropuerto de origen"
+            className="bg-amber-900"
+            onChange={(e) => setOrigCode(e.target.value)}
+            />
+
+            <p>Aeropuerto de destino:</p>
+            <input
+            type= "text"
+            placeholder="Aeropuerto de destino"
+            className="bg-amber-900"
+            onChange={(e) => setDestCode(e.target.value)}
+            />
+
+            <p>Fecha de salida:</p>
+            <input
+            type= "date"
+            placeholder="Fecha de salida"
+            className="bg-amber-900"
+            onChange={(e) => setDepartDate(e.target.value)}
+            />
+
+            <p>Fecha de regreso:</p>
+            <input
+            type= "date"
+            placeholder="Fecha de regreso"
+            className="bg-amber-900"
+            onChange={(e) => setReturnDate(e.target.value)}
+            />
+
+            <p>Numero de personas:</p>
+            <input
+            type= "number"
+            placeholder="Numero de personas"
+            className="bg-amber-900"
+            onChange={(e) => setPeopleQty(e.target.value)}
+            />
+
+
+
+            <p>Holiiiis</p>
+            <button type="submit">Search</button>
+
+        </form>
+        </>
+    )
+}
